f99295ce073d82972323691b8db042ced415bbf7
serhdemidov <serhdemidov@softkit.company>
2021-04-14 14:39:02 +0300

    [RT-4] basic repo creation

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+const App = (): JSX.Element => {
+  return (
+    <div className="App">
+      <div>Hello, worlds</div>
+    </div>
+  );
+};
+
+export default App;
