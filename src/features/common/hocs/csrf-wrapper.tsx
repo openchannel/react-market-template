24becf268b25c81865666113e6898f9702b3ff35
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-08-04 12:49:42 +0300

    [RT-89] refactor: add auth/csrf/oidc required hocs

--- src/features/common/hocs/csrf-wrapper.tsx
+++ src/features/common/hocs/csrf-wrapper.tsx
@@ -3,29 +3,20 @@ import { auth } from '@openchannel/react-common-services';
 
 type CsrfWrapper = {
 	children: JSX.Element;
-}
+};
 
-export const CsrfWrapper: React.FC<CsrfWrapper> = (props) => {
-	const [isLoaded, setIsLoaded] = React.useState(false);
-
+export const CsrfWrapper: React.FC<CsrfWrapper> = ({ children }) => {
 	React.useEffect(() => {
 		const fetchCsrf = async () => {
 			try {
 				await auth.initCsrf();
-				setIsLoaded(true)
 			} catch (error) {
-				console.error('error', error);
+				console.error('CsrfWrapper fetchCsrf error', error);
 			}
 		}
 
 		fetchCsrf();
 	}, []);
 
-	if (!isLoaded) {
-		return (
-			<div className="bg-container">Loading...</div>
-		);
-	}
-
-	return props.children;
+	return children;
 };
