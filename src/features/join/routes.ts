408c75439900d31eb1dedef0bdd3e93aa34c0df4
serhdemidov <serhdemidov@softkit.company>
2021-08-12 19:10:02 +0300

    Hero component markup and styles with dummy data

--- src/features/join/routes.ts
+++ src/features/join/routes.ts
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const LoginPage = React.lazy(() => import('./pages/login/login'));
+const MainPage = React.lazy(() => import('../common/pages/main/main'));
 
 export const joinRoutes = [
 	{
@@ -8,5 +9,11 @@ export const joinRoutes = [
 		exact: true,
 		protected: true,
 		Component: LoginPage,
+	},
+{
+		path: '/',
+		exact: true,
+		protected: true,
+		Component: MainPage,
 	},
 ];
