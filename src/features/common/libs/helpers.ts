25ccf405384e3b18cb1ab87823d02a06f0ce79ad
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-08-18 19:20:01 +0300

    [RT-100] fix: use lodash fn instead of custom fn

--- src/features/common/libs/helpers.ts
+++ src/features/common/libs/helpers.ts
@@ -1,10 +1,7 @@
-export const isEmpty = <T>(value: T) => {
-	if (typeof value === 'string') return value.length === 0;
-	if (typeof value === 'number') return value === 0;
-	if (Array.isArray(value)) return value.length === 0;
-	return !value;
-}
+import isEmpty from 'lodash.isempty';
 
 export const isNonEmpty = <T>(value: T) => {
 	return !isEmpty(value);
 };
+
+export { isEmpty };
