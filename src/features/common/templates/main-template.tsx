26b5693c0c5739b7c66e244447c6ab7d83d9e082
vladimir.fedorchenko <onevovan@softkitit.com>
2021-08-25 11:51:35 +0300

    [RT-100] upd: remove wrong useless useCmsData in footer. pass cmsData to getStarted component

--- src/features/common/templates/main-template.tsx
+++ src/features/common/templates/main-template.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { OcFooter } from '@openchannel/react-common-components/dist/ui/common/organisms';
 
-import { useCmsData } from '../hooks';
+import { useTypedSelector } from '../hooks';
 import { socialLinks } from '../../../consts/social-links';
 
 export const MainTemplate: React.FC = ({ children }) => {
-	const { footer } = useCmsData();
+	const footer = useTypedSelector(({ cmsContent }) => cmsContent.footer);
 
 	return (
 		<>
