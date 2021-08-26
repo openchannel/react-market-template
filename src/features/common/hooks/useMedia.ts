724a2b6a13fffb8ed8079d67107436c4894a9106
vladimir.fedorchenko <onevovan@softkitit.com>
2021-08-26 14:09:00 +0300

    [RT-95] add: hook useMedia

---
+++ src/features/common/hooks/useMedia.ts
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+export function useMedia() {
+    const [value, setValue] = React.useState(false);
+    React.useEffect(
+        () => {
+            const mediaQueryList = window.matchMedia('(max-width: 768px)');
+            const handler = () => setValue(mediaQueryList.matches);
+
+            mediaQueryList.addEventListener('change', handler);
+            handler();
+
+            return () => mediaQueryList.removeEventListener('change', handler);
+        },
+        []
+    );
+    return value;
+}
