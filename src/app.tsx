fab8b04b622d418769bddccf2d94d1766e40ab47
serhdemidov <serhdemidov@softkit.company>
2021-08-26 15:54:37 +0300

    Added progress-bar to market

--- src/app.tsx
+++ src/app.tsx
@@ -4,19 +4,19 @@ import { BrowserRouter } from 'react-router-dom';
 import { instance } from '@openchannel/react-common-services';
 import { OcNotificationContainer } from '@openchannel/react-common-components/dist/ui/common/atoms';
 
+import { initProgressBar } from '@openchannel/react-common-services/dist';
 import { CsrfWrapper } from './features/common/hocs';
 import { Helmet } from './features/common/molecules';
 import { Routes } from './routes';
 import { store } from './store';
 
 import './features/common/libs/interceptors';
-import { initProgressBar } from 'features/common/libs/interceptors/progress-bar';
 import '@openchannel/react-common-components'; // it's styles
 import './theme.scss';
 import './styles.scss';
 
 instance.init({ url: process.env.REACT_APP_API_URL || '', headerName: 'X-CSRF-TOKEN' });
-initProgressBar({ showSpinner: false });
+initProgressBar();
 
 export const App = (): JSX.Element => {
   return (
--- src/features/common/libs/interceptors/index.ts
+++ src/features/common/libs/interceptors/index.ts
@@ -1,2 +1,1 @@
 import './error-handler';
-import './progress-bar';
