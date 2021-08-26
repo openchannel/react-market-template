ba1d1620c067f47b88eba402387382ac02c45d49
vladimir.fedorchenko <onevovan@softkitit.com>
2021-08-26 15:47:45 +0300

    [RT-95] fix: added header in main-template

--- src/features/common/pages/home/index.tsx
+++ src/features/common/pages/home/index.tsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 
 import { AppList } from '../../organisms';
 import { MainTemplate } from '../../templates';
-import { Hero, GetStarted, Header } from '../../components';
+import { Hero, GetStarted } from '../../components';
 
 import './style.scss';
 
 export const HomePage = () => {
   return (
     <MainTemplate>
-      <Header />
       <Hero />
       <AppList />
       <GetStarted />
