ed0f04e9609f13fd5fafe83c015c61a3b9965c8a
vladimir.fedorchenko <onevovan@softkitit.com>
2021-08-27 11:55:11 +0300

    [RT-125] upd: add useAuth hook to store session

--- src/features/common/templates/main-template.tsx
+++ src/features/common/templates/main-template.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
 import { OcFooter } from '@openchannel/react-common-components/dist/ui/common/organisms';
 
-import { useTypedSelector } from '../hooks';
-import { socialLinks } from '../../../consts/social-links';
 import { Header } from '../components';
+import { useTypedSelector, useAuth } from '../hooks';
+import { socialLinks } from '../../../consts/social-links';
 
 export const MainTemplate: React.FC = ({ children }) => {
   const { header, footer } = useTypedSelector(({ cmsContent }) => cmsContent);
+  const { checkSession } = useAuth();
+
+  React.useEffect(() => {
+    const check = async () => {
+      try {
+        await checkSession();
+      } catch {
+        // do nothing
+      }
+    };
+
+    check();
+  }, []);
 
   return (
     <>
