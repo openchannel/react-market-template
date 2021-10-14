6faf72ca154d2cf0a34d68d27d1da0fc441b5a90
vladimir.fedorchenko <onevovan@softkitit.com>
2021-10-14 20:07:56 +0300

    [RT-130] add: prop process

--- src/features/join/pages/forgot-password/index.tsx
+++ src/features/join/pages/forgot-password/index.tsx
@@ -4,16 +4,19 @@ import forgotPasswordDoneIcon from '../../../../../public/assets/img/forgot-pass
 import { OcForgotPasswordComponent } from '@openchannel/react-common-components/dist/ui/auth/organisms';
 import { errorMessages, email, isEmptyInputValue } from '@openchannel/react-common-components/dist/ui/form/lib';
 import { resetPassword } from '../../../common/store/session/actions';
+import { useDispatch } from 'react-redux';
 import './styles.scss';
 
 const ForgotPassword = (): JSX.Element => {
   const [inputValue, setInputValue] = React.useState('');
   const [inputError, setInputError] = React.useState('');
   const [showResultPage, setShowResultPage] = React.useState(false);
+  const [loadingRequest, setLoadingRequest] = React.useState(false);
+  const dispatch = useDispatch();
 
-  const invalidMassage = () => errorMessages.email();
-  const requiredMassage = () => errorMessages.required();
-  const validateEmail = () => email();
+  const invalidMassage = React.useCallback(() => errorMessages.email(), []);
+  const requiredMassage = React.useCallback(() => errorMessages.required(), []);
+  const validateEmail = React.useCallback(() => email(), []);
 
   const onChange = (e: { target: HTMLInputElement }) => {
     setInputValue(e.target.value);
@@ -28,14 +31,16 @@ const ForgotPassword = (): JSX.Element => {
   };
 
   const onSubmit = async () => {
+    setLoadingRequest(true);
     if (validateEmail()(inputValue) === null && !isEmptyInputValue(inputValue)) {
-      await resetPassword(inputValue);
+      await dispatch(resetPassword(inputValue));
       setShowResultPage(true);
       setInputValue('');
     }
     if (isEmptyInputValue(inputValue)) {
       setInputError(requiredMassage);
     }
+    setLoadingRequest(false);
   };
 
   return (
@@ -53,6 +58,7 @@ const ForgotPassword = (): JSX.Element => {
           signupUrl="/signup"
           onSubmit={onSubmit}
           inputError={inputError}
+          process={loadingRequest}
         />
       </div>
     </div>
