b03ea3352033ccad5743881185879dcc46b33436
vladimir.fedorchenko <onevovan@softkitit.com>
2021-10-18 18:52:23 +0300

    [RT-133] add: constants

--- src/features/join/pages/forgot-password/constants.ts
+++
@@ -1,5 +0,0 @@
-import { email, errorMessages } from '@openchannel/react-common-components/dist/ui/form/lib';
-
-export const invalidMassage = () => errorMessages.email();
-export const validateEmail = () => email();
-export const requiredField = () => errorMessages.required();
