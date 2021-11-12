86e94603c140b5f9bc3514bdf84eb466619438f5
vladimir.fedorchenko <onevovan@softkitit.com>
2021-11-12 15:21:38 +0200

    [RT-165] upd: action

--- src/features/apps/store/apps/actions.ts
+++ src/features/apps/store/apps/actions.ts
@@ -131,11 +131,20 @@ export const fetchGalleries = () => async (dispatch: Dispatch) => {
   }
 };
 
-export const getFeaturedApps = (sort: string, filter: string) => async (dispatch: Dispatch) => {
-  const {
-    data: { list },
-  } = await apps.getApps(1, 4, sort, filter, true);
-  dispatch(setFeatured(list));
+export const getFeaturedApps = () => async (dispatch: Dispatch) => {
+  dispatch(startLoading());
+  const featuredSort = JSON.stringify({ randomize: 1 });
+  const featuredFilter = JSON.stringify({ 'attributes.featured': 'yes' });
+  try {
+    const {
+      data: { list },
+    } = await apps.getApps(1, 4, featuredSort, featuredFilter, true);
+    dispatch(setFeatured(list));
+  } catch (error) {
+    dispatch(finishLoading());
+
+    throw error;
+  }
 };
 
 export const fetchFilters = () => async (dispatch: Dispatch) => {
